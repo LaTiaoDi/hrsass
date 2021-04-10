b82734c560b0afd7467d33fc313a5a1b8a12aa66
laitaodi <7606711+latiaodi@user.noreply.gitee.com>
2021-04-10 14:09:28 +0800

    day3完成

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,26 @@
+import { login } from "@/api/user";
+import * as Storage from "@/utils/auth";
+const state = {
+  token: Storage.getToken()
+};
+const mutations = {
+  setToken(state, token) {
+    state.token = token;
+    Storage.setToken(token);
+  }
+};
+const actions = {
+  async login(context, data) {
+    const result = await login(data);
+
+    context.commit("setToken", result);
+  }
+};
+
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+
+  actions
 };
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,24 @@
 // 导出一个axios的实例  而且这个实例要有请求拦截器 响应拦截器
 import axios from "axios";
-const service = axios.create(); // 创建一个axios的实例
+import { Message } from "element-ui";
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API, //基础地址
+  timeout: 5000 // 超时
+}); // 创建一个axios的实例
 service.interceptors.request.use(); // 请求拦截器
-service.interceptors.response.use(); // 响应拦截器
+service.interceptors.response.use(
+  response => {
+    const { data, message, success } = response.data;
+    if (success) {
+      return data;
+    } else {
+      Message(message);
+      return Promise.reject(new Error(message));
+    }
+  },
+  error => {
+    Message.error(error.message);
+    return Promise.reject(error); //返回
+  }
+); // 响应拦截器
 export default service; // 导出axios实例
